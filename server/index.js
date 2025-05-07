1ff0f4f667b6036ee897d3b025621a3c7381da5c
goofyjnr <goofyjnr@gmail.com>
2025-05-07 15:30:23 +1200

    Innitial commit
    Setting up some of the stuff needed
    -still need to set up the react app
    -still need to set up the actual server

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+//listen to the port to start the server
+app.listen(5000, () =>{
+    console.log("server started on port 5000");
+});
+//to start the server use node index 
+
