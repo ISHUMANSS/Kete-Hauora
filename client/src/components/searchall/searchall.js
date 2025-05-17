ffad93f91864c2ed6476cfe42bc4e75b54ec4459
goofyjnr <goofyjnr@gmail.com>
2025-05-17 16:09:00 +1200

    now can view all the entered items in the db on the page

---
+++ client/src/components/searchall/searchall.js
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from "react";
+
+
+const SearchAll = () => {
+
+    const [services, setServices] = useState([]);
+
+    const getSessions = async() => {
+        try {
+            //make the request
+            const response = await fetch("http://localhost:5000/services");
+
+            const jsonData = await response.json();
+
+            setServices(jsonData);
+            
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    useEffect(() => {
+        //run to fetch the data each time the page is rendered
+        getSessions();
+    },[]);
+
+
+
+
+    return(
+        <div className="SearchAll">
+
+            <h1>Search for all the services: </h1>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Company name</th>
+                    
+                </tr>
+                </thead>
+                <tbody>
+                {services.map(service =>(
+                    <tr>
+                        <td>{service.service_id}</td>
+                        <td>{service.company_name}</td>
+                    </tr>
+                ))}
+
+                </tbody>
+            </table>
+            
+        </div>
+    );
+};
+
+export default SearchAll;
