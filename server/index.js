1ae018b8c2294e183da8405940233e9cc933b868
goofyjnr <goofyjnr@gmail.com>
2025-05-17 14:45:25 +1200

    Created the React app
    React app has been created
    
    some of the needed routes to search and interact with postgreSQL have been started in the server folder

--- server/index.js
+++ server/index.js
@@ -1,11 +1,55 @@
 const express = require("express");//make sure express is installed
 const app = express();//run express
 const cors = require("cors");//make sure cors is needed
-
+const pool = require("./db");//allows us to make postgres querys
 
 //middleware
 app.use(cors());//run cors
 app.use(express.json());//gives us access to request the data
+//use req.body to be able to access the info from the client side
+
+//routes
+//all of the actions that we will have
+//req is the request
+//res is the response
+
+//use app.post along with async functions to post a service
+
+//get all listings
+app.get("/services", async(req,res) =>{
+    //uses _GET
+    try {
+        const allServices = await pool.query("SELECT * FROM service;");
+        res.json(allServices.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+
+//search for a specific listing
+app.get("/services:id", async (req,res) =>{
+    //search for a listing by id
+    //uses _GET
+    try {
+        const {id} = req.params;//set the id as the one in the url
+
+        //serach for the service where it matches the id given
+        const service = await pool.query("SELECT * FROM services WHERE service_id = $1", [id]);
+        
+        res.json(service.row[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+
+//update a listing
+//use app.put
+
+//delete a listing?
+//use app.delete
+
 
 
 //listen to the port to start the server
