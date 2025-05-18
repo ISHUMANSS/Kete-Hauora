21a0c71a69120a8c3afe811acabc0c539e51f539
Kimju <rfp6097@autuni.ac.nz>
2025-05-18 20:40:06 +1200

    add homepage

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require("express");//make sure express is installed
 const app = express();//run express
 const cors = require("cors");//make sure cors is needed
-
+const path = require("path")
 
 //middleware
 app.use(cors());//run cors
 app.use(express.json());//gives us access to request the data
 
+app.use(express.static("public")); // serve static files like style.css
+
+app.get("/", (_, res) => {
+    res.sendFile(path.join(__dirname, "homepage.html", "public")) // route for homepage
+});
 
 //listen to the port to start the server
-app.listen(5000, () =>{
-    console.log("server started on port 5000");
+app.listen(3000, () => {
+    console.log("server started on port 3000");
 });
 //to start the server use node index 
 
