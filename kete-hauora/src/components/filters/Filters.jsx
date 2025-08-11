87b5d6ba53dda260d6ffa6c9e3fe8d2f68a1df5c
Is_human <goofyjnr@gmail.com>
2025-08-11 15:13:12 +1200

    currently able to get all the cats that are in the database and have them in the listr

--- kete-hauora/src/components/filters/Filters.jsx
+++ kete-hauora/src/components/filters/Filters.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Filters.css';
+
+import supabase from '../../config/supabaseClient';
 
 //filters component box where the user is able to select the filters and have that update the filters they are currently using
 //now we can just add some more filters into this box 
 
 const FiltersBox = ({ filters, setFilters }) => {
+
+    const [categories, setCategories] = useState([]);
+    //const [languages, setLanguages] = useState([]);
+
+    //get all the filters from the db
+    useEffect(() => {
+        const fetchFilterData = async () => {
+            //fetch categories
+            //to do this I had to allow for a RLS in supabase
+            const { data: catData, error: catError } = await supabase
+                .from('categories')
+                .select('category_id, category')
+                .order('category', { ascending: true });
+
+            if (catError) {
+                console.error('Error fetching categories:', catError.message);
+            } else {
+                console.log('Fetched categories:', catData); // debug log
+                setCategories(catData);
+            };
+
+            /*
+            //not sure if we wnt to get the languages from the database
+            //fetch languages
+            const { data: langData, error: langError } = await supabase
+                .from('languages')
+                .select('language_id, language')
+                .order('language', { ascending: true });
+
+            if (!langError) setLanguages(langData);
+            */
+
+            //I can just add the regions here also
+        };
+        
+
+        fetchFilterData();
+    }, []);
+
 
     //click the buttons to reset the filters
     const handleClearFilters = () => {
@@ -30,16 +71,16 @@ const FiltersBox = ({ filters, setFilters }) => {
         <div className="filters-section">
             <div className="filter-group">
                 <label>Category</label>
-                
                 <select
                     value={filters.category}
-                    
-                    onChange={(e) => setFilters(prev => ({ ...prev, category: e.target.value })) /*allows us to change the filter list*/}
+                    onChange={(e) => setFilters(prev => ({ ...prev, category: e.target.value }))}
                 >
                     <option value="">All Categories</option>
-                    <option value="Health">Health</option>
-                    <option value="Education">Education</option>
-                    <option value="Community">Community</option>
+                    {categories.map((cat) => (
+                    <option key={cat.category_id} value={cat.category_id}>
+                        {cat.category}
+                    </option>
+                    ))}
                 </select>
             </div>
 
