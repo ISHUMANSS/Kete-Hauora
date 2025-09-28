e1cbae63a7bfb680a31dba302770aa83b91643de
Tuitauofiti <qws7323@autuni.ac.nz>
2025-09-28 19:44:23 +1300

    Update navbar
    Completed navbar UI

--- kete-hauora/src/components/navbar/navbar.js
+++ kete-hauora/src/components/navbar/navbar.js
@@ -1,58 +1,89 @@
 /* eslint-disable no-undef */
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.css';
-import { useTranslation } from 'react-i18next';
-import i18next from 'i18next';
-import i18n from '../../i18n';
-import logo from '../../assets/stacked_logo_white.svg';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+import { useTranslation } from "react-i18next";
+import i18next from "i18next";
+import i18n from "../../i18n";
+import logo from "../../assets/stacked_logo_white.svg";
 
 const Navbar = () => {
   const { t } = useTranslation();
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => setSidebarOpen(prev => !prev);
+  const toggleSidebar = () => setSidebarOpen((prev) => !prev);
 
-  const handleLanguageChange = (e) => { // runs whenever new option is selected from language dropdown and gets value of selected item
+  const handleLanguageChange = (e) => {
+    // runs whenever new option is selected from language dropdown and gets value of selected item
     const newLang = e.target.value;
     // eslint-disable-next-line no-undef
     i18n.changeLanguage(newLang);
-    localStorage.setItem("i18nextLng", newLang);//remembers selection
+    localStorage.setItem("i18nextLng", newLang); //remembers selection
   };
 
   return (
     <nav className="navbar">
-      <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="Middlemore Foundation Logo" className="logo-img" />
-        </Link>
+      <div className="nav-container">
+        <div className="logo">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="Middlemore Foundation Logo"
+              className="logo-img"
+            />
+          </Link>
+        </div>
+
+        <div className="nav-right">
+          <ul className="nav-links top-row">
+            <li>
+              <Link to="/about">{t("About")}</Link>
+            </li>
+            <li>
+              <Link to="/login">
+                <span className="login-icon material-symbols-outlined">
+                  person
+                </span>
+                {t("Login")}
+              </Link>
+            </li>
+            <li>
+              <div className="language-dropdown">
+                <select
+                  onChange={handleLanguageChange}
+                  value={i18next.language}
+                >
+                  <option value="en">English</option>
+                  <option value="mi">Maori</option>
+                </select>
+                <span className="dropdown-icon material-symbols-outlined">
+                  arrow_drop_down
+                </span>
+              </div>
+            </li>
+          </ul>
+
+          <div className="divider"></div>
+
+          <ul className="nav-links bottom-row">
+            <li>
+              <Link to="/contact">{t("Contact Us")}</Link>
+            </li>
+            <li>
+              <Link to="/services">{t("Find A Service")}</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </div>
       </div>
 
       <div className="hamburger" onClick={toggleSidebar}>
         ☰
       </div>
 
-      <ul className={`nav-links ${sidebarOpen ? 'open' : ''}`}>
-        <li><Link to="/about">{t("About")}</Link></li>
-        <li><Link to="/contact">{t("Contact Us")}</Link></li>
-        <li><Link to="/services">{t("Find A Service")}</Link></li>
-        <li>
-          <Link to="/login">
-            <span className="login-icon material-symbols-outlined">person</span>{t("Login")}
-          </Link>
-        </li>
-        <li><Link to="/admin">Admin</Link></li>
-
-        {/*change site language dropdown*/}
-        <li>
-          <select onChange={handleLanguageChange} value={i18next.language}>
-            <option value="en">English</option>
-            <option value="mi">Maori</option>
-          </select>
-        </li>
-      </ul>
-         {sidebarOpen && <div className="overlay" onClick={toggleSidebar}></div>}
-
+      {sidebarOpen && <div className="overlay" onClick={toggleSidebar}></div>}
     </nav>
   );
 };
